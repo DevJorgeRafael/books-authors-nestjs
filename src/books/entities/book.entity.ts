cefeaa9339433f638814e097ad8076b6a48922bd
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2025-01-11 18:38:12 -0500

    config books module

---
+++ src/books/entities/book.entity.ts
@@ -0,0 +1,26 @@
+import { AuthorsBook } from "src/authors-books/entities/authors-book.entity";
+import { PrimaryGeneratedColumn, Column, OneToMany, Entity } from "typeorm";
+
+@Entity('books')
+export class Book {
+    @PrimaryGeneratedColumn({ name: 'book_id' })
+    id: number;
+
+    @Column({ name: 'book_title' })
+    title: string;
+
+    @Column({ name: 'book_year' })
+    year: number;
+
+    @Column({ name: 'book_literary_genre' })
+    literary_genre: string;
+
+    @Column({ name: 'book_publication_date' })
+    publication_date: Date;
+
+    @Column({ name: 'book_language' })
+    language: string;
+
+    @OneToMany(() => AuthorsBook, (authorsBook) => authorsBook.book)
+    authorsBooks: AuthorsBook[];
+}
